f81d2b46ad867d07ebfce320369c22ebb4c7b6e0
Suyesh Bhatta <suyesh.bhatta199@gmail.com>
2025-04-08 07:48:10 -0500

    Initial Commit, API setup and created receipts model.

---
+++ Models/Receipt.cs
@@ -0,0 +1,11 @@
+namespace ReimbursementAPI.Models
+{
+    public class Receipt
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Amount { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public string FileName { get; set; } = string.Empty;
+    }
+}
